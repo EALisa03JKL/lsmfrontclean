b721b47433bbee25de87d5ac48c4221024db2f6f
EALisa03JKL <elisaguadalupea.fim@ms.uas.edu.mx>
2024-11-19 21:37:18 -0700

    hola

--- src/app/dictionary/ui/ui.component.ts
+++ src/app/dictionary/ui/ui.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-ui',
   standalone: true,
   imports: [],
   templateUrl: './ui.component.html',
-  styleUrl: './ui.component.css'
+  styleUrl: './ui.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class UiComponent {
-
+  
 }
