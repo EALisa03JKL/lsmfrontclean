79560b46b430b5a21158cae641627d0655612d91
EALisa03JKL <elisaguadalupea.fim@ms.uas.edu.mx>
2025-01-14 12:48:27 -0700

    correcciones

--- server.js
+++ server.js
@@ -1,10 +1,54 @@
 const express = require('express');
-const app = express();
-const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
-app.use(express.static(__dirname + '/angularapp'));
-app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {
-res.sendFile(path.join(__dirname + '/angularapp/index.html'));
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const rooms = {}; // Objeto para almacenar las salas y sus usuarios
+
+io.on('connection', (socket) => {
+  console.log('Un usuario se conectó:', socket.id);
+
+  socket.on('create_room', (roomCode) => {
+    if (!rooms[roomCode]) {
+      rooms[roomCode] = [];
+      console.log(`Sala creada: ${roomCode}`);
+    }
+
+    if (rooms[roomCode].length < 2) {
+      rooms[roomCode].push(socket.id);
+      socket.join(roomCode);
+      io.to(roomCode).emit('room_update', rooms[roomCode]); // Actualizar usuarios en la sala
+    } else {
+      socket.emit('error', 'La sala ya está llena');
+    }
+  });
+
+  socket.on('join_room', (roomCode) => {
+    if (rooms[roomCode] && rooms[roomCode].length < 2) {
+      rooms[roomCode].push(socket.id);
+      socket.join(roomCode);
+      io.to(roomCode).emit('room_update', rooms[roomCode]);
+    } else {
+      socket.emit('error', 'La sala no existe o está llena');
+    }
+  });
+
+  socket.on('disconnect', () => {
+    for (const roomCode in rooms) {
+      const index = rooms[roomCode].indexOf(socket.id);
+      if (index !== -1) {
+        rooms[roomCode].splice(index, 1);
+        if (rooms[roomCode].length === 0) delete rooms[roomCode];
+        else io.to(roomCode).emit('room_update', rooms[roomCode]);
+        console.log(`Usuario desconectado de sala ${roomCode}:`, socket.id);
+      }
+    }
+  });
 });
-console.log('Build successful!!');
+
+server.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:3000');
+});
